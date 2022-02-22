0261dde8af15b5ba44dd7ff8216eff7018986fd7
Guilherme Gasparotti Cassettari <guizaocassettari@gmail.com>
2022-02-22 19:46:43 -0300

    fix(homepage) resolve exercise

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,39 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import { Button } from './Button';
+
+interface Genre {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface Props {
+  genres: Genre[]
+  handleClickButton: Function
+  selectedGenreId: number
+}
+
+
+export function Content(props: Props) {
+  
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {props.genres.map(genre  => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => props.handleClickButton(genre.id)}
+          selected={props.selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+    </nav>
+
+  )
 }
